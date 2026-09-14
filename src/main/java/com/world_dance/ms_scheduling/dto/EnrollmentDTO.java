1a72e46a0a5a47b87d7a3354d8123c669ed561fa
JuanDiego110508 <juandiegolr28@gmail.com>
2026-09-14 11:02:33 -0500

    refactor: Se cambio variable de Id para flujo Feing

--- src/main/java/com/world_dance/ms_scheduling/dto/EnrollmentDTO.java
+++ src/main/java/com/world_dance/ms_scheduling/dto/EnrollmentDTO.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EnrollmentDTO {
+    @com.fasterxml.jackson.annotation.JsonProperty("enrollmentId")
     private Long id;
     private Long userId;
     private Long eventId;
